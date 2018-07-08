6b2d77ded833cb76dcb3a5fd5dac78aab1a07af6
JasmineCarter <jlcart6@gmail.com>
2018-07-07 22:10:23 -0400

    first mvc fail

--- src/test/java/org/wecancodeit/reviewsitefullstack/ReviewControllerMockMvcTest.java
+++ src/test/java/org/wecancodeit/reviewsitefullstack/ReviewControllerMockMvcTest.java
@@ -1,25 +1,64 @@
 package org.wecancodeit.reviewsitefullstack;
 
-public class ReviewControllerMockMvcTest {
-}
-	
-//	@Test //also tag and category
-//	public void shouldRouteToSingleCourseView() throws Exception {
-//		
-//	}
-//	
-//	@Test //also tag and category
-//	public void shouldRouteToAllReviewView() throws Exception {
-//		
-//	}
-//	
-//	@Test //also tag and category
-//	public void shouldBeOkforSingleReview() throws Exception {
-//		
-//	}
-//	
-//	@Test //also tag and category
-//	public void shouldBeOkForAllReview() throws Exception {
-//}
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-	
+import java.util.Optional;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ReviewController.class)
+public class ReviewControllerMockMvcTest {
+
+
+
+	@Resource
+	private MockMvc mvc;
+
+	@MockBean
+	private ReviewRepository reviewRepo;
+
+	@Mock
+	private Review review;
+
+	@Test // also tag and category
+	public void shouldRouteToSingleReviewView() throws Exception {
+		long reviewId = 1;
+		when(reviewRepo.findById(reviewId)).thenReturn(Optional.of(review));
+		mvc.perform(get("/review?id=1")).andExpect(view().name(is("reviewTemplate")));
+	}
+}
+
+// @Test
+// public void shouldRouteToSingleCategoryView() throws Exception {
+// long reviewId = 1;
+// when(reviewRepo.findById(reviewId)).thenReturn(Optional.of(review));
+// mvc.perform(get("/review?id=1")).andExpect(view().name(is("reviewTemplate")));
+//
+// @Test // also tag and category
+// public void shouldRouteToAllReviewView() throws Exception {
+//
+// }
+//
+// @Test // also tag and category
+// public void shouldBeOkforSingleReview() throws Exception {
+//
+// }
+//
+// @Test // also tag and category
+// public void shouldBeOkForAllReview() throws Exception {
+// }
+// }
